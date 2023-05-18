b9d41b59d6fda7fcac75f961e37f296f2a9140b6
lovelydett <xyt@bupt.cn>
2023-05-18 06:49:26 -0400

    [C++] producer_consumer cv + lck

--- src/language/cpp/thread/producer_consumer/implement1.cpp
+++ src/language/cpp/thread/producer_consumer/implement1.cpp
@@ -14,6 +14,8 @@
 #include <mutex>
 #include <vector>
 #include <cassert>
+#include <cstdio>
+#include <cstdlib>
 
 std::mutex mtx;
 std::condition_variable cv;
@@ -35,7 +37,55 @@ void producer() {
         // 1. left_count < MAX
         // 2. we are holding the lock.
         assert(left_count < MAX);
-    } 
-    
+        left_count++;
+        printf("(");
+
+        // We then notify all consumers and producers.
+        cv.notify_all();
+    }
 }
 
+/**
+ * @brief consumer prints right quote ')'
+ *
+ */
+void consumer() {
+    while (true) {
+        std::unique_lock<std::mutex> lck(mtx);
+        while (left_count <= 0) {
+            cv.wait(lck);
+        }
+        // As long as we reach here, two things are dead true:
+        // 1. left_count > 0
+        // 2. we are holding the lock.
+        assert(left_count > 0);
+        left_count--;
+        printf(")");
+
+        // We then notify all consumers and producers.
+        cv.notify_all();
+    }
+}
+
+int main(int argc, char const *argv[])
+{
+    int num_threads;
+    if (argc > 1) {
+        MAX = atoi(argv[1]);
+        num_threads = atoi(argv[2]);
+        assert(MAX > 0 && num_threads > 0);
+    } else {
+        MAX = 10;
+        num_threads = 10;
+    }
+    std::vector<std::thread> threads;
+    threads.reserve(num_threads * 2);
+    for (int i = 0; i < num_threads; i++) {
+        threads.push_back(std::thread(producer));
+        threads.push_back(std::thread(consumer));
+    }
+    for (auto &t : threads) {
+        t.join();
+    }
+    return 0;
+}
