7b60ae7f91b8257f9d75a883f08b3f51b05db2d5
lovelydett <xyt@bupt.cn>
2024-01-16 18:23:40 +0800

    [Java] maven

---
+++ src/languages/java/maven_app/src/main/java/com/lovelydett/app/App.java
@@ -0,0 +1,13 @@
+package com.lovelydett.app;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
